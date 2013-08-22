2e821bb402027a3285cd11006d38f931dca385c4
Sandy Vanderbleek <sandy@cardspring.com>
2013-08-22 20:35:54 +0000

    pass rule specs

--- lib/stagiaire/rule.rb
+++ lib/stagiaire/rule.rb
@@ -1,9 +1,23 @@
 class Stagiaire
   class Rule
-    def files
+    def files(directory)
+      file_targets.inject([]) do |files, file_target|
+        files << Dir[File.join(directory, file_target)]
+      end.flatten
     end
 
-    def changes
+    def changes(files)
+      files.inject([]) do |changes, file|
+        File.open(file).each_line do |line|
+          line_rules.each do |line_rule|
+            if line_rule.first.match(line)
+              changes << [line.chomp, line.gsub(line_rule.first, line_rule.last).chomp]
+            end
+          end
+        end
+
+        changes
+      end
     end
   end
 end
--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 require 'stagiaire/rule'
+require 'tmpdir'
+require 'tempfile'
 
 describe Stagiaire::Rule do
   class TestRule < Stagiaire::Rule
     def file_targets
-      ['*.rb']
+      ['matching*']
     end
 
     def line_rules
@@ -14,22 +16,32 @@ describe Stagiaire::Rule do
 
   let(:matching_line) { 'test line' }
   let(:not_matching_line) { 'line' }
-  let(:matching) {}
-  let(:not_matching) {}
+  let!(:matching) do 
+    file = Tempfile.new('matching')
+    file.write([matching_line, not_matching_line].join("\n"))
+    file.rewind
+    file
+  end
+  let!(:not_matching) { Tempfile.new('not_matching') }
+  let(:directory) { Dir.tmpdir }
 
   it 'finds matching files' do
-    expect(TestRule.new.files).to include(matching)
+    expect(TestRule.new.files(directory)).to include(matching.path)
   end
 
   it 'does not find files that do not match' do
-    expect(TestRule.new.files).to_not include(not_matching)
+    expect(TestRule.new.files(directory)).to_not include(not_matching.path)
   end
 
-  it 'run on matching lines in matching files' do
-    expect(TestRule.new.changes).to include(matching_line, matching_line.gsub(*TestRule.line_rules.to_a.flatten))
-  end
+  context 'with files' do
+    let(:files) { [matching] }
 
-  it 'does not run on lines that do not match in matching files' do
-    expect(TestRule.new.changes).to_not include(not_matching_line)
+    it 'run on matching lines in matching files' do
+      expect(TestRule.new.changes(files).flatten).to include(matching_line, matching_line.gsub(*TestRule.new.line_rules.to_a.flatten))
+    end
+
+    it 'does not run on lines that do not match in matching files' do
+      expect(TestRule.new.changes(files).flatten).to_not include(not_matching_line)
+    end
   end
 end
